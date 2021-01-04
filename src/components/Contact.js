2d3f7d5c40a966a053e2fcf57318ceab1ec82e82
T <gauquelintimothe@gmail.com>
2021-01-04 09:04:16 +0100

    Update Contact Component
    Create titleInsert & contentInsert

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,34 +1,51 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
 import { FormattedMessage } from 'react-intl';
+
 import '../stylesheets/Contact.scss';
+import Button from 'react-bootstrap/Button';
+import { EnvelopeFill, Globe2, TelephoneFill } from 'react-bootstrap-icons';
 
 const Contact = () => {
     return (
         <div className="whiteCard row">
 
-            <div className="description col-lg-7">
-                <h1><b>Ravi de vous voir ici !</b></h1>
-                <h2>Car cela me prouve que vous avez pris du temps pour me connaitre.
-                Et rien que cela, ça me va droit au coeur !
-                </h2>
-                <div className="request">Je suis actuellement à la recherche d’un stage de développeur Web
-                et Web Mobile pour en apprendre toujours plus
+            <div className="leftBlock col-lg-5">
+                <div className="titleInsert">
+                    <FormattedMessage
+                        id="contact.welcomeBack"
+                        defaultMessage="Ravi de vous revoir !"
+                    />
                 </div>
-                <h2 className="row">
-                    <p>timothegauquelin@gmail.com</p>
-                </h2>
-                <div className="row">
-
+                <div className="contentInsert">
+                    <FormattedMessage
+                        id="contact.itProvesToMe"
+                        defaultMessage="Cela me prouve que vous avez pris du temps pour me connaître !"
+                    />
                 </div>
-                <div className="row">
-
+                <div className="contentInsert contentInsert--redBold">
+                    <FormattedMessage
+                        id="contact.internship"
+                        defaultMessage="Je suis actuellement à la recherche d'un stage"
+                    />
                 </div>
+                <ul>
+                    <li>
+                        <EnvelopeFill className="icon" color="#2b78c5" size={20} />
+                        timothegauquelin@gmail.com
+                    </li>
+                    <li>
+                        <TelephoneFill className="icon" color="#2b78c5" size={20} />
+                        06.41.97.99.90
+                    </li>
+                    <li>
+                        <Globe2 className="icon" color="#2b78c5" size={20} />
+                    </li>
+                </ul>
 
 
             </div>
-            <div className="col-lg">
-                <Button className="bouton" size="lg">
+            <div className="rightBlock col-lg">
+                <Button className="buttonInsert" size="lg">
                     <a href={`${process.env.PUBLIC_URL}/img/cv.pdf`} tartget="_blank">
                         <FormattedMessage
                             id="contact.downloadCV"
